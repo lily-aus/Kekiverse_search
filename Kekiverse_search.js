15ad5838f6f6d4d255639861b5736705b9938e0d
lili-du <lily1206.du@gmail.com>
2022-10-29 01:32:14 +1100

    Search function done for Kekiverse_search

---
+++ Kekiverse_search.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+// load the kekData.json file
+let jsonData = require('./kekData.json');
+console.log(jsonData);
+
+
+// Create the dictionary for search
+const kekDict = new Object();
+for (data of jsonData) {
+    kekDict[data.noun] = data
+};
+console.log(kekDict);
+
+// search function
+app.get('/search', (req, res) => {
+
+    // decode URL
+    const decoded_q = decodeURIComponent(req.query.q)
+
+    // check if the keyword is in kekDict
+    if (decoded_q in kekDict) {
+        res.send(kekDict[decoded_q])  // return the value if found
+    } else {
+        res.sendStatus(404)  // return 404 if not found
+    }
+
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})

c8f8d7859e50b59849f24b825a7267febf90f2e1
lili-du <lily1206.du@gmail.com>
2022-11-09 22:13:39 +1100

    added URL redirect and URL search in the URL bar

--- Kekiverse_search.js
+++ Kekiverse_search.js
@@ -1,6 +1,15 @@
+/* 
+ * Name: Lili Du
+ * Email: dulil@oregonstate.edu
+*/
+
 const express = require('express')
 const app = express()
 app.use(express.static('public'))
+
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs.engine({ defaultLayout: null }));
+app.set('view engine', 'handlebars');
 
 const port = 3000
 
@@ -27,12 +36,32 @@ app.get('/search', (req, res) => {
 
     // check if the keyword is in kekDict
     if (decoded_q in kekDict) {
-        res.send(kekDict[decoded_q])  // return the value if found
+        encoded_q = encodeURI(decoded_q)
+        res.redirect('/' + encoded_q)  // redirect it to the result page
+
     } else {
         res.sendStatus(404)  // return 404 if not found
     }
 
 })
+
+app.get('/:noun', (req, res) => {
+    // decode URL
+    const decoded_noun = decodeURIComponent(req.params.noun)
+
+    // check if the keyword is in kekDict
+    if (decoded_noun in kekDict) {
+        res.status(200).render('noun', {
+            RenderShit: true,
+            kekData: kekDict[decoded_noun]
+
+        })
+    } else {
+        res.status(404).render('noun', {
+            RenderShit: false
+        })  // return 404 if not found
+    }
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
